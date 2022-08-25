3ecbf1364bdfdb87407f6d7047a26771bb987aa3
asad9284 <asad94758@gmail.com>
2022-08-25 17:06:49 +0530

    first commit

---
+++ src/Component/Quote.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+
+const url = "https://api.quotable.io/random?=author";
+
+const Quote = () => {
+
+
+  const [quotes, setQuotes] = useState([]);
+  const getQuote = () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setQuotes(data));
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getNewQuote = () => {
+    getQuote();
+  };
+
+
+
+
+  const { content, author } = quotes;
+
+  return (
+    <div >
+      <div className="container" >
+        <div className="row ">
+          <div className=" col-4">
+          <div class="card text-center">
+ 
+  <div class="card-body">
+    <h4>{author}</h4>
+    <p className="card-text">{content}</p>
+    <button className="btn btn-primary" onClick={getNewQuote}>New Quote</button>
+  </div>
+</div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Quote;
